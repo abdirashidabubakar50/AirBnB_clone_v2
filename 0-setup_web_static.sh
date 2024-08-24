2e341dc6bb5f270129cee967b5fa74c3222cbfeb
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-08-24 13:03:40 +0300

    preparing the webservers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,42 +1,40 @@
 #!/usr/bin/env bash
-# sets up the webserver for the deployment of  web_static
+# Sets up the web server for the deployment of web_static
 
-# install nginx if not already installed
-if ! nginx -v &> /dev/null
-then
-    sudo apt-get update
-    sudo apt-get instal nginx -y
-fi
+# Install nginx if not already installed
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo ufw allow 'Nginx HTTP'
 
-# create required directories of they don't already exist
-sudo mkdir -p /data/
-sudo mkdir p /data/web_static
-sudo mkdir -p /data/web_static/releases
-sudo mkdir -p /data/web_static/shared
+# Create required directories if they don't already exist
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-# create a fake html file to test Nginx configuration
-sudo echo "<html>
+sudo mkdir -p /data/web_static/shared
+
+# Create a fake HTML file to test Nginx configuration
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>
+EOF
 
-# create symbolic link to test the folder
+# Create symbolic link to test the folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# give ownership of the /data/ folder to the ubuntu user and group
+# Give ownership of the /data/ folder to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# update the nginx configuration to serve the content
+# Update the Nginx configuration to serve the content
 nginx_conf="/etc/nginx/sites-available/default"
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' $nginx_conf
+if ! grep -q "location /hbnb_static/" $nginx_conf; then
+    sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' $nginx_conf
+fi
 
-
-# Restart nginx to apply the changes
+# Restart Nginx to apply the changes
 sudo service nginx restart
 
-# ensure the script executes successfully
+# Ensure the script executes successfully
 exit 0
