7cb04062ef750612963d67938eddcf6e9b732d49
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-08-25 15:42:37 +0300

    display all the cities

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+app.teardown_appcontext
+
+
+def teardown():
+    storage.close
+
+
+@app.route('/states_list/', strict_slashes=False)
+def list_states():
+    """Display the list of all objects present in DB storage
+    sorted b name (A - Z) and a description of each state"""
+    all_states = storage.all('State')
+
+    sorted_states = sorted(all_states.values(), key=lambda state: state.name)
+
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
